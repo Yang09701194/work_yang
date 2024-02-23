261513a1bff9100a572c4cdb9bddb750d49739f1
Yang09701194 <mustnecessity@gmail.com>
2024-02-23 16:27:14 +0800

    springboot application

---
+++ springapp/src/main/java/app/DemoApplication.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+
+
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@SpringBootApplication
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
